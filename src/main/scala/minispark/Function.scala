cf4f9141d116b8d2d0a012051182f70a1cff564e
Tomasz Michniewski <tmichniewski@gmail.com>
2021-11-28 16:45:19 +0100

    Add definition of the Function

---
+++ src/main/scala/minispark/Function.scala
@@ -0,0 +1,24 @@
+package com.github
+package minispark
+
+import org.apache.spark.sql.Dataset
+
+/**
+ * The function represents any kind of transformation of one Dataset into another.
+ * Such functions are present in any Spark notebook,
+ * but here we define a type for such functions.
+ * We call this type the Function.
+ *
+ * @tparam T Type of input data.
+ * @tparam U Type of output data.
+ */
+trait Function[T, U] extends (Dataset[T] => Dataset[U]) {
+  /**
+   * Sequential application of two functions. An alias to andThen method.
+   *
+   * @param f The function which will be applied next.
+   * @tparam V Type of resulting data.
+   * @return Returns the composed function.
+   */
+  def +[V](f: Function[U, V]): Function[T, V] = (d: Dataset[T]) => (this andThen f)(d) // d transform (this andThen f)
+}
