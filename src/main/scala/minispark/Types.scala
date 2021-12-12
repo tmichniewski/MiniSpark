4656f4138a344e4391f01692ccbefd9d0c991266
Tomasz Michniewski <tmichniewski@gmail.com>
2021-12-12 17:54:33 +0100

    Remove deprecated methods

--- src/main/scala/minispark/Types.scala
+++ src/main/scala/minispark/Types.scala
@@ -7,8 +7,6 @@ import org.apache.spark.sql.Dataset
 
 trait F0[T] extends (() => Dataset[T]) {
   def +[U](f0u: F0[U]): F0F0[T, U] = F0F0[T, U](this, f0u) // F0 + F0 = (F0, F0)
-  @deprecated("Use F0[T].+[U](f0u: F0[U]): F0F0[T, U] and F0F0[T, U].+[V](f2tuv: F2[T, U, V]): F0[V] instead")
-  def +[U, V](f0u: F0[U], f2tuv: F2[T, U, V]): F0[V] = () => f2tuv(apply(), f0u()) // F0 + (F0, F2) = F0
   def +[U](f1tu: F1[T, U]): F0[U] = () => f1tu(apply()) // F0 + F1 = F0
   def +[U, V](f2tuv: F2[T, U, V]): F1[U, V] = f2tuv(apply(), _) // F0 + F2 = F1
 }
@@ -29,7 +27,6 @@ trait F2[T, U, V] extends ((Dataset[T], Dataset[U]) => Dataset[V]) {
   def +[W](f1vw: F1[V, W]): F2[T, U, W] = (d1: Dataset[T], d2: Dataset[U]) => f1vw(apply(d1, d2)) // F2 + F1 = F2
 }
 
-@deprecated("Use Seq(ds1, ds2,..., dsN).reduce(_ union _) instead (plain Scala only)")
 trait FN[T, U] extends (Seq[Dataset[T]] => Dataset[U]) {
   def +[W](f1uw: F1[U, W]): FN[T, W] = (ds: Seq[Dataset[T]]) => f1uw(apply(ds)) // FN + F1 = FN
 }
