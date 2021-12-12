4b1e62d851d3ad54f178474d648a01ddb9d812d9
Tomasz Michniewski <tmichniewski@gmail.com>
2021-12-12 08:29:15 +0100

    Extend and refactor types

--- src/main/scala/minispark/Types.scala
+++ src/main/scala/minispark/Types.scala
@@ -4,34 +4,37 @@ package minispark
 import org.apache.spark.sql.Dataset
 
 trait F0[T] extends (() => Dataset[T]) {
-  def +[U](f0: F0[U]): F0F0[T, U] = F0F0[T, U](this, f0) // F0 + F0 = (F0, F0)
-  def +[U](f1: F1[T, U]): F0[U] = () => f1(apply()) // F0 + F1 = F0
-  def +[U, V](f2: F2[T, U, V]): F1[U, V] = f2(apply(), _) // F0 + F2 = F1
+  def +[U](f0u: F0[U]): F0F0[T, U] = F0F0[T, U](this, f0u) // F0 + F0 = (F0, F0)
+  //def +[U, V](f0u: F0[U], f2tuv: F2[T, U, V]): F0[V] = () => f2tuv(apply(), f0u()) // F0 + (F0, F2) = F0
+  def +[U](f1tu: F1[T, U]): F0[U] = () => f1tu(apply()) // F0 + F1 = F0
+  def +[U, V](f2tuv: F2[T, U, V]): F1[U, V] = f2tuv(apply(), _) // F0 + F2 = F1
 }
 
-class F0F0[T, U](f01: F0[T], f02: F0[U]) {
-  def +[V, W](f11: F1[T, V], f12: F1[U, W]): F0F0[V, W] = F0F0[V, W](f01 + f11, f02 + f12) // (F0, F0) + (F1, F1) = (F0, F0)
-  def +[V](f2: F2[T, U, V]): F0[V] = () => f2(f01(), f02()) // (F0, F0) + F2 = F0
+class F0F0[T, U](f0t: F0[T], f0u: F0[U]) {
+  def +[V, W](f1tv: F1[T, V], f1uw: F1[U, W]): F0F0[V, W] = F0F0[V, W](f0t + f1tv, f0u + f1uw) // (F0, F0) + (F1, F1) = (F0, F0)
+  def +[V](f2tuv: F2[T, U, V]): F0[V] = () => f2tuv(f0t(), f0u()) // (F0, F0) + F2 = F0
 }
 object F0F0 {
-  def apply[T, U](f01: F0[T], f02: F0[U]) = new F0F0[T, U](f01, f02)
+  def apply[T, U](f0t: F0[T], f0u: F0[U]) = new F0F0[T, U](f0t, f0u)
 }
+// F0 + F0 + F2 = F0
 
 trait F1[T, U] extends (Dataset[T] => Dataset[U]) {
-  def +[V](f: F1[U, V]): F1[T, V] = (d: Dataset[T]) => f(apply(d)) // F1 + F1 = F1
+  def +[V](f1uv: F1[U, V]): F1[T, V] = (d: Dataset[T]) => f1uv(apply(d)) // F1 + F1 = F1
 }
 
 trait F2[T, U, V] extends ((Dataset[T], Dataset[U]) => Dataset[V]) {
-  def +[W](f: F1[V, W]): F2[T, U, W] = (d1: Dataset[T], d2: Dataset[U]) => f(apply(d1, d2)) // F2 + F1 = F2
+  def +[W](f1vw: F1[V, W]): F2[T, U, W] = (d1: Dataset[T], d2: Dataset[U]) => f1vw(apply(d1, d2)) // F2 + F1 = F2
 }
 
 trait FN[T, U] extends (Seq[Dataset[T]] => Dataset[U]) {
-  def +[V](f: F1[U, V]): FN[T, V] = (ds: Seq[Dataset[T]]) => f(apply(ds)) // FN + F1 = FN
+  def +[V](f1uv: F1[U, V]): FN[T, V] = (ds: Seq[Dataset[T]]) => f1uv(apply(ds)) // FN + F1 = FN
 }
 
+@deprecated
 trait Reducer[T, U] {
   def apply(ds: Dataset[T]*): Dataset[U]
-  def +[V](f: F1[U, V]): Reducer[T, V] = (ds: Dataset[T]*) => f(apply(ds: _*)) // Reduce + F1 = Reduce
+  //def +[V](f1uv: F1[U, V]): Reducer[T, V] = (ds: Dataset[T]*) => f1uv(apply(ds: _*)) // Reduce + F1 = Reduce
 }
 
 // X + F1 = X
