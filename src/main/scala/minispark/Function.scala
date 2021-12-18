f0d28960d9bdfce832c3c74e60fe2c22c86df337
Tomasz Michniewski <tmichniewski@gmail.com>
2021-12-18 14:12:38 +0100

    Update Function definition

--- src/main/scala/minispark/Function.scala
+++ src/main/scala/minispark/Function.scala
@@ -1,6 +1,8 @@
 package com.github
 package minispark
 
+import org.apache.spark.sql.{Dataset, Row}
+
 /**
  * The function represents any kind of transformation of one Dataset into another.
  * Such functions are present in any Spark notebook,
@@ -10,17 +12,15 @@ package minispark
  * @tparam T Type of input data.
  * @tparam U Type of output data.
  */
-trait Function[T, U] extends F1[T, U] with Serializable
-
-// Subsequent implementation would be valid if there were no Types and inheritance from F1
-// trait Function[T, U] extends (Dataset[T] => Dataset[U]) with F1[T, U] with Serializable {
-//   /**
-//    * Sequential application of two functions. An alias to andThen method.
-//    *
-//    * @param f The function which will be applied next.
-//    * @tparam V Type of resulting data.
-//    * @return Returns the composed function.
-//    */
-//   def +[V](f: Function[U, V]): Function[T, V] = (d: Dataset[T]) => d.transform(this andThen f)
-//   // <=> (this andThen f)(d) or andThen(f)(d) or f(apply(d))
-// }
+trait Function[T, U] extends (Dataset[T] => Dataset[U]) with F1[T, U] with Serializable {
+  /**
+   * Sequential application of two functions. An alias to andThen method.
+   *
+   * @param f The function which will be applied next.
+   * @tparam V Type of resulting data.
+   * @return Returns the composed function.
+   */
+  def +[V](f: Function[U, V]): Function[T, V] = (d: Dataset[T]) => d.transform(this andThen f)
+  // <=> (this andThen f)(d) or andThen(f)(d) or f(apply(d))
+  // Please note that there is also a similar method on superclass F1 which returns F1
+}
