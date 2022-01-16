dc0aa017f51e23cb4d934c2534653a759ea36923
Tomasz Michniewski <tmichniewski@gmail.com>
2022-01-16 09:08:26 +0100

    Rename the types (experimentally)

--- src/main/scala/minispark/Types.scala
+++ src/main/scala/minispark/Types.scala
@@ -5,41 +5,41 @@ import org.apache.spark.sql.Dataset
 
 trait ETL extends (() => Unit)
 
-trait Extract[T] extends (() => Dataset[T]) {
-  def +[U](extract: Extract[U]): ExtractPair[T, U] = ExtractPair[T, U](this, extract) // E + E = E2
-  def +[U](transform: Transform[T, U]): Extract[U] = () => transform(apply()) // E + T = E
-  def +(load: Load[T]): ETL = () => load(apply()) // E + L = ETL
-  def split: Split[T] = Split(this)
-  def *[U, V](combine: Combine[T, U, V]): Transform[U, V] = combine(apply(), _) // E + C = T
-  def *[U, V](combine: Combine[U, T, V]): Transform[U, V] = combine(_, apply()) // E + C = T
+trait E[X] extends (() => Dataset[X]) {
+  def +[Y](e: E[Y]): E2[X, Y] = E2[X, Y](this, e) // E + E = E2
+  def +[Y](t: T[X, Y]): E[Y] = () => t(apply()) // E + T = E
+  def +(l: L[X]): ETL = () => l(apply()) // E + L = ETL
+  def split: S[X] = S(this)
+  def *[Y, Z](c: C[X, Y, Z]): T[Y, Z] = c(apply(), _) // E + C = T
+  def *[Y, Z](c: C[Y, X, Z]): T[Y, Z] = c(_, apply()) // E + C = T
 }
 // E + E * C = E + T = E
 
-case class ExtractPair[T, U](e1: Extract[T], e2: Extract[U]) {
-  def +[V](combine: Combine[T, U, V]): Extract[V] = () => combine(e1(), e2()) // E + E + C = E
+case class E2[X, Y](e1: E[X], e2: E[Y]) {
+  def +[Z](c: C[X, Y, Z]): E[Z] = () => c(e1(), e2()) // E + E + C = E
 }
 
-case class Split[T](e: Extract[T]) extends Extract[T] {
-  lazy val d: Dataset[T] = e().cache()
-  override def apply(): Dataset[T] = d
+case class S[X](e: E[X]) extends E[X] {
+  lazy val d: Dataset[X] = e().cache()
+  override def apply(): Dataset[X] = d
 }
 
-trait Transform[T, U] extends (Dataset[T] => Dataset[U]) {
-  def +[V](transform: Transform[U, V]): Transform[T, V] = (d: Dataset[T]) => transform(apply(d)) // T + T = T
-  def +(load: Load[U]): Load[T] = (d: Dataset[T]) => load(apply(d)) // T + L = L
-  def ++[V, W](combine: Combine[U, V, W]): Combine[T, V, W] = (d1: Dataset[T], d2: Dataset[V]) => combine(apply(d1), d2) // T + C = C
-  def ++[V, W](combine: Combine[V, U, W]): Combine[V, T, W] = (d1: Dataset[V], d2: Dataset[T]) => combine(d1, apply(d2)) // T + C = C
+trait T[X, Y] extends (Dataset[X] => Dataset[Y]) {
+  def +[Z](t: T[Y, Z]): T[X, Z] = (d: Dataset[X]) => t(apply(d)) // T + T = T
+  def +(l: L[Y]): L[X] = (d: Dataset[X]) => l(apply(d)) // T + L = L
+  def ++[Z, W](c: C[Y, Z, W]): C[X, Z, W] = (d1: Dataset[X], d2: Dataset[Z]) => c(apply(d1), d2) // T + C = C
+  def ++[Z, W](c: C[Z, Y, W]): C[Z, X, W] = (d1: Dataset[Z], d2: Dataset[X]) => c(d1, apply(d2)) // T + C = C
 }
 // (T + T) ++ C = T + (T ++ C) ?
 
-trait Load[T] extends (Dataset[T] => Unit)
+trait L[X] extends (Dataset[X] => Unit)
 
-trait Combine[T, U, V] extends ((Dataset[T], Dataset[U]) => Dataset[V]) {
-  def +[W](transform: Transform[V, W]): Combine[T, U, W] = (d1: Dataset[T], d2: Dataset[U]) => transform(apply(d1, d2)) // C + T = C
-  def ++(load: Load[V]): (Dataset[T], Dataset[U]) => Unit = (d1: Dataset[T], d2: Dataset[U]) => load(apply(d1, d2)) // C + L = L2
+trait C[X, Y, Z] extends ((Dataset[X], Dataset[Y]) => Dataset[Z]) {
+  def +[W](t: T[Z, W]): C[X, Y, W] = (d1: Dataset[X], d2: Dataset[Y]) => t(apply(d1, d2)) // C + T = C
+  def ++(l: L[Z]): (Dataset[X], Dataset[Y]) => Unit = (d1: Dataset[X], d2: Dataset[Y]) => l(apply(d1, d2)) // C + L = L2
 }
 
 @deprecated
 object types {
-  def join[T, U, V](e1: Extract[T], e2: Extract[U], combine: Combine[T, U, V]): Extract[V] = () => combine(e1(), e2())
+  def join[X, Y, Z](e1: E[X], e2: E[Y], combine: C[X, Y, Z]): E[Z] = () => combine(e1(), e2())
 }
