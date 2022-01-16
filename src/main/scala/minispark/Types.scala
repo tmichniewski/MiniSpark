931c3e83f555f0c31052a24fea81eaa05bfe88a6
Tomasz Michniewski <tmichniewski@gmail.com>
2022-01-16 08:53:56 +0100

    Add Split

--- src/main/scala/minispark/Types.scala
+++ src/main/scala/minispark/Types.scala
@@ -9,6 +9,7 @@ trait Extract[T] extends (() => Dataset[T]) {
   def +[U](extract: Extract[U]): ExtractPair[T, U] = ExtractPair[T, U](this, extract) // E + E = E2
   def +[U](transform: Transform[T, U]): Extract[U] = () => transform(apply()) // E + T = E
   def +(load: Load[T]): ETL = () => load(apply()) // E + L = ETL
+  def split: Split[T] = Split(this)
   def *[U, V](combine: Combine[T, U, V]): Transform[U, V] = combine(apply(), _) // E + C = T
   def *[U, V](combine: Combine[U, T, V]): Transform[U, V] = combine(_, apply()) // E + C = T
 }
@@ -16,6 +17,11 @@ trait Extract[T] extends (() => Dataset[T]) {
 
 case class ExtractPair[T, U](e1: Extract[T], e2: Extract[U]) {
   def +[V](combine: Combine[T, U, V]): Extract[V] = () => combine(e1(), e2()) // E + E + C = E
+}
+
+case class Split[T](e: Extract[T]) extends Extract[T] {
+  lazy val d: Dataset[T] = e().cache()
+  override def apply(): Dataset[T] = d
 }
 
 trait Transform[T, U] extends (Dataset[T] => Dataset[U]) {
@@ -33,6 +39,7 @@ trait Combine[T, U, V] extends ((Dataset[T], Dataset[U]) => Dataset[V]) {
   def ++(load: Load[V]): (Dataset[T], Dataset[U]) => Unit = (d1: Dataset[T], d2: Dataset[U]) => load(apply(d1, d2)) // C + L = L2
 }
 
+@deprecated
 object types {
   def join[T, U, V](e1: Extract[T], e2: Extract[U], combine: Combine[T, U, V]): Extract[V] = () => combine(e1(), e2())
 }
