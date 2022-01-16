7198f85bb47db5f4141d81554f4aa497bc135872
Tomasz Michniewski <tmichniewski@gmail.com>
2022-01-16 09:20:33 +0100

    Rename back the parameters to full names

--- src/main/scala/minispark/Types.scala
+++ src/main/scala/minispark/Types.scala
@@ -6,25 +6,25 @@ import org.apache.spark.sql.Dataset
 trait ETL extends (() => Unit)
 
 trait Extract[X] extends (() => Dataset[X]) {
-  def +[Y](e: Extract[Y]): ExtractPair[X, Y] = ExtractPair[X, Y](this, e) // E + E = E2
-  def +[Y](t: Transform[X, Y]): Extract[Y] = () => t(apply()) // E + T = E
-  def +(l: Load[X]): ETL = () => l(apply()) // E + L = ETL
+  def +[Y](extract: Extract[Y]): ExtractPair[X, Y] = ExtractPair[X, Y](this, extract) // E + E = E2
+  def +[Y](transform: Transform[X, Y]): Extract[Y] = () => transform(apply()) // E + T = E
+  def +(load: Load[X]): ETL = () => load(apply()) // E + L = ETL
   def split: Split[X] = Split(this)
 }
 // E + E * C = E + T = E
 
-case class ExtractPair[X, Y](e1: Extract[X], e2: Extract[Y]) {
-  def +[Z](c: Combine[X, Y, Z]): Extract[Z] = () => c(e1(), e2()) // E + E + C = E
+case class ExtractPair[X, Y](extract1: Extract[X], extract2: Extract[Y]) {
+  def +[Z](combine: Combine[X, Y, Z]): Extract[Z] = () => combine(extract1(), extract2()) // E + E + C = E
 }
 
-case class Split[X](e: Extract[X]) extends Extract[X] {
-  lazy val d: Dataset[X] = e().cache()
+case class Split[X](extract: Extract[X]) extends Extract[X] {
+  lazy val d: Dataset[X] = extract().cache()
   override def apply(): Dataset[X] = d
 }
 
 trait Transform[X, Y] extends (Dataset[X] => Dataset[Y]) {
-  def +[Z](t: Transform[Y, Z]): Transform[X, Z] = (d: Dataset[X]) => t(apply(d)) // T + T = T
-  def +(l: Load[Y]): Load[X] = (d: Dataset[X]) => l(apply(d)) // T + L = L
+  def +[Z](transform: Transform[Y, Z]): Transform[X, Z] = (d: Dataset[X]) => transform(apply(d)) // T + T = T
+  def +(load: Load[Y]): Load[X] = (d: Dataset[X]) => load(apply(d)) // T + L = L
 }
 // (T + T) ++ C = T + (T ++ C) ?
 
