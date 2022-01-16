c464b67454294564b74e9e64d17daeda15b3a850
Tomasz Michniewski <tmichniewski@gmail.com>
2022-01-16 09:17:47 +0100

    Rename back the types to full names

--- src/main/scala/minispark/Types.scala
+++ src/main/scala/minispark/Types.scala
@@ -5,29 +5,29 @@ import org.apache.spark.sql.Dataset
 
 trait ETL extends (() => Unit)
 
-trait E[X] extends (() => Dataset[X]) {
-  def +[Y](e: E[Y]): E2[X, Y] = E2[X, Y](this, e) // E + E = E2
-  def +[Y](t: T[X, Y]): E[Y] = () => t(apply()) // E + T = E
-  def +(l: L[X]): ETL = () => l(apply()) // E + L = ETL
-  def split: S[X] = S(this)
+trait Extract[X] extends (() => Dataset[X]) {
+  def +[Y](e: Extract[Y]): ExtractPair[X, Y] = ExtractPair[X, Y](this, e) // E + E = E2
+  def +[Y](t: Transform[X, Y]): Extract[Y] = () => t(apply()) // E + T = E
+  def +(l: Load[X]): ETL = () => l(apply()) // E + L = ETL
+  def split: Split[X] = Split(this)
 }
 // E + E * C = E + T = E
 
-case class E2[X, Y](e1: E[X], e2: E[Y]) {
-  def +[Z](c: C[X, Y, Z]): E[Z] = () => c(e1(), e2()) // E + E + C = E
+case class ExtractPair[X, Y](e1: Extract[X], e2: Extract[Y]) {
+  def +[Z](c: Combine[X, Y, Z]): Extract[Z] = () => c(e1(), e2()) // E + E + C = E
 }
 
-case class S[X](e: E[X]) extends E[X] {
+case class Split[X](e: Extract[X]) extends Extract[X] {
   lazy val d: Dataset[X] = e().cache()
   override def apply(): Dataset[X] = d
 }
 
-trait T[X, Y] extends (Dataset[X] => Dataset[Y]) {
-  def +[Z](t: T[Y, Z]): T[X, Z] = (d: Dataset[X]) => t(apply(d)) // T + T = T
-  def +(l: L[Y]): L[X] = (d: Dataset[X]) => l(apply(d)) // T + L = L
+trait Transform[X, Y] extends (Dataset[X] => Dataset[Y]) {
+  def +[Z](t: Transform[Y, Z]): Transform[X, Z] = (d: Dataset[X]) => t(apply(d)) // T + T = T
+  def +(l: Load[Y]): Load[X] = (d: Dataset[X]) => l(apply(d)) // T + L = L
 }
 // (T + T) ++ C = T + (T ++ C) ?
 
-trait L[X] extends (Dataset[X] => Unit)
+trait Load[X] extends (Dataset[X] => Unit)
 
-trait C[X, Y, Z] extends ((Dataset[X], Dataset[Y]) => Dataset[Z])
+trait Combine[X, Y, Z] extends ((Dataset[X], Dataset[Y]) => Dataset[Z])
